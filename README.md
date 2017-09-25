466123500719aef3181be689295a49bb415caee6
Paweł Pietralik <Pawel.Pietralik@firma.interia.pl>
2017-09-25 08:19:57 +0200

    initializing repo

---
+++ README.md
@@ -0,0 +1,1 @@
+# Tumbrl 404 harvester created by Pawel Pietralik -- 2017-09-25
---
+++ TumbrlHarvester/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Utworzone przez SharpDevelop.
+ * Użytkownik: pawel.pietralik
+ * Data: 2017-09-25
+ * Godzina: 08:13
+ * 
+ * Do zmiany tego szablonu użyj Narzędzia | Opcje | Kodowanie | Edycja Nagłówków Standardowych.
+ */
+using System;
+
+namespace TumbrlHarvester
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
